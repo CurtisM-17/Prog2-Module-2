cc8eaa9c23740ac1417a651306ac3ffa2366efed
Curtis M <curtis11macleod@gmail.com>
2024-01-31 11:01:22 -0500

    Task 2: Delete trail and lerp aircraft size on spacebar

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Plane : MonoBehaviour
 {
 	public List<Vector2> points;
-	public float pointThreshold = 0.2f;
+	public float pointThreshold = 0.1f;
 	Vector2 lastPos;
 	LineRenderer lr;
+	Vector2 currentPos;
+	Rigidbody2D rb;
+	public float speed = 1f;
+	public AnimationCurve landing;
+	float landingTimer = 0f;
 
 	private void Start()
 	{
+		rb = GetComponent<Rigidbody2D>();
 		lr = GetComponent<LineRenderer>();
+
 		lr.positionCount = 1;
 		lr.SetPosition(0, transform.position);
 	}
@@ -36,6 +43,46 @@ public class Plane : MonoBehaviour
 			lr.SetPosition(lr.positionCount - 1, currentPos);
 
 			lastPos = currentPos;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		currentPos = new Vector2(transform.position.x, transform.position.y);
+
+		if (points.Count > 0)
+		{
+			Vector2 direction = points[0] - currentPos;
+			float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+
+			rb.rotation = -angle;
+		}
+
+		rb.MovePosition(rb.position + (Vector2)transform.up * speed * Time.deltaTime);
+	}
+
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			landingTimer += 0.1f * Time.deltaTime;
+			float lerp = landing.Evaluate(landingTimer);
+			if (transform.localScale.z < 0.1f)
+			{
+				Destroy(gameObject);
+			}
+			transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, lerp);
+		}
+
+		if (points.Count > 0 && Vector2.Distance(currentPos, points[0]) < pointThreshold)
+		{
+			points.RemoveAt(0);
+
+			for (int i = 0; i < lr.positionCount - 2; i++)
+			{
+				lr.SetPosition(i, lr.GetPosition(i+1));
+			}
+			lr.positionCount--;
 		}
 	}
 }
