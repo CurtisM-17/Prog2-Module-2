d077f2c466bb19e50e160b0e01a35f22cb1bbbe0
Curtis M <curtis11macleod@gmail.com>
2024-02-14 10:30:13 -0500

    Task 2: Scene B

---
+++ Assets/Week 6/Scripts/SpaceToContinue.cs
@@ -0,0 +1,20 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpaceToContinue : MonoBehaviour
+{
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+			int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+
+			SceneManager.LoadScene(nextSceneIndex);
+		}
+	}
+}
