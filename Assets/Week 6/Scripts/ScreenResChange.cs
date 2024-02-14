9cb6fe5680d56fa40a582a2a0737dfe69c0f5334
Curtis M <curtis11macleod@gmail.com>
2024-02-14 11:24:03 -0500

    Task 4a) Make the UI

---
+++ Assets/Week 6/Scripts/ScreenResChange.cs
@@ -0,0 +1,16 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenResChange : MonoBehaviour
+{
+	public void ChangeRes(bool fullRes)
+	{
+		if (fullRes) Screen.SetResolution(1920, 1080, false);
+		else Screen.SetResolution(1920, 720, false);
+
+		Debug.Log(Screen.currentResolution);
+	}
+}
